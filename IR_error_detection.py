2c2b647d3b3189ce2f202b05a96893f86fecea3c
AndyFreeman <95344446+AndyFreeeeman@users.noreply.github.com>
2022-12-21 13:58:49 +0800

    Update IR_error_detection.py

--- IR_error_detection.py
+++ IR_error_detection.py
@@ -1,30 +1,20 @@
-# 樓下cv2版串流
-
-from Detection import Detection,drawBbox
-detection = Detection(weight="./product-detect-single-best-221208.pt",im0_shape=(720,1280))
-
-import numpy as np
-import cv2
-import time
 import os
 
-class_path = 'F:\\7-11\\train\\'
-error_path = 'E:\\output_img1\\'
+home_path = '/opt/product-recognition-711/record/'
 
-all_class_path = os.listdir(class_path)
 
-for class_counter in range(len(all_class_path)):
-    
-    print(all_class_path[class_counter])
-    print(str(class_counter) + " / " + str(len(all_class_path)))
-    all_img_path = os.listdir(class_path + all_class_path[class_counter])
-    
-    for all_img_counter in range(len(all_img_path)):
+os.chdir(home_path)
+all_img_path = os.listdir(os.curdir)
+all_img_path = all_img_path[1:]
+
+for img_path_counter in range( 0 , len(all_img_path) - 1 ):
+    for in_out_counter in range(0,2):
+        current_path = home_path + str(all_img_path[img_path_counter])
         
-        current_path = class_path + all_class_path[class_counter] + "//" + all_img_path[all_img_counter]
-        
-        frame = cv2.imread(current_path)
+        if not os.path.isdir(current_path+"\\slot_in"):
+            print(str(all_img_path[img_path_counter]))
+            print("no slot_in")
             
-        bbox = detection.inference(frame)
-        
-        print(bbox)
+        if not os.path.isdir(current_path+"\\slot_out"):
+            print(str(all_img_path[img_path_counter]))
+            print("no slot_out")
